4640bba2a109e8874edf24e06c0964950031e19b
Brian Chevalier <>
2020-11-26 23:11:53 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matrix-compare "0.1.0-SNAPSHOT"
+(defproject matrix-compare "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
